c28c0bf73b61004e6f2bacedef4bdb6644355e6d
Brian Lusina <lusinabrian@gmail.com>
2017-11-10 15:49:57 +0300

    add card components to questionnaire

--- src/components/questionnaire/Questionnaire.js
+++ src/components/questionnaire/Questionnaire.js
@@ -1,12 +1,36 @@
 /**
  * @author lusinabrian on 10/11/17.
- * @notes:
+ * @notes: Questionnaire component that will contain quesionnaire form
+ *
  */
 
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Card, {CardContent} from "material-ui/Card"
+import { withStyles } from "material-ui/styles";
 
-export default class Questionnaire extends Component {
+const styles = theme => ({
+    card: {
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        marginBottom: 16,
+        fontSize: 14,
+        color: theme.palette.text.secondary,
+    },
+    pos: {
+        marginBottom: 12,
+        color: theme.palette.text.secondary,
+    },
+});
+
+
+class Questionnaire extends Component {
     constructor(props, context) {
         super(props, context);
 
@@ -14,8 +38,16 @@ export default class Questionnaire extends Component {
     }
 
     render() {
+        const { classes } =  this.props;
+
         return (
-            <div>{this.props.myProps}</div>
+            <div>
+                <Card className={classes.card}>
+                    <CardContent>
+
+                    </CardContent>
+                </Card>
+            </div>
         );
     }
 }
@@ -23,3 +55,5 @@ export default class Questionnaire extends Component {
 Questionnaire.propTypes = {
     myProps: PropTypes.string.isRequired
 };
+
+export default withStyles(styles)(Questionnaire)
