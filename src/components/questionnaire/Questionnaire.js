bbc6f49ccb8b4e711a7153651e61dd15a5e24e53
Brian Lusina <lusinabrian@gmail.com>
2017-11-10 23:25:05 +0300

    Create questionnaire form

--- src/components/questionnaire/Questionnaire.js
+++ src/components/questionnaire/Questionnaire.js
@@ -7,12 +7,24 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Card, {CardContent} from "material-ui/Card"
-import { withStyles } from "material-ui/styles";
+import {withStyles} from "material-ui/styles";
+import TextField from 'material-ui/TextField';
+import {FormControl} from "material-ui/Form";
+import Select from "material-ui/Select";
+import Input, {InputLabel} from "material-ui/Input";
+import Button from "material-ui/Button";
+import Grid from "material-ui/Grid"
+
 
 const styles = theme => ({
     card: {
         minWidth: 275,
     },
+
+    button: {
+        margin: theme.spacing.unit,
+    },
+
     bullet: {
         display: 'inline-block',
         margin: '0 2px',
@@ -27,6 +39,10 @@ const styles = theme => ({
         marginBottom: 12,
         color: theme.palette.text.secondary,
     },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 100,
+    },
 });
 
 
@@ -38,13 +54,84 @@ class Questionnaire extends Component {
     }
 
     render() {
-        const { classes } =  this.props;
+        const {classes} = this.props;
 
         return (
             <div>
                 <Card className={classes.card}>
                     <CardContent>
+                        <Grid container spacing={24}>
+                            <Grid item xs={12}>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="type-of-shop">
+                                        Type of shop
+                                    </InputLabel>
+                                    <Select
+                                        autowidth={true}
+                                        native
+                                        input={
+                                            <Input id="type-of-shop"/>
+                                        }>
+                                        <option value=""/>
+                                        <option value={10}>Ten</option>
+                                        <option value={20}>Twenty</option>
+                                        <option value={30}>Thirty</option>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
 
+                            <Grid item xs={12}>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="shop-sell-airtime">
+                                        Does the shop sell airtime?
+                                    </InputLabel>
+                                    <Select
+                                        native
+                                        input={
+                                            <Input id="shop-sell-airtime"/>
+                                        }>
+                                        <option value=""/>
+                                        <option value={"yes"}>YES</option>
+                                        <option value={"no"}>NO</option>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="mobile-money-available">
+                                        Is mobile money available?
+                                    </InputLabel>
+                                    <Select
+                                        native
+                                        input={
+                                            <Input id="mobile-money-available"/>
+                                        }>
+                                        <option value=""/>
+                                        <option value={"yes"}>YES</option>
+                                        <option value={"no"}>NO</option>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="name"
+                                    label="Agents Comments"
+                                    type="text"
+                                    fullWidth
+                                />
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <Button raised color="primary"
+                                        className={classes.button}>
+                                    Submit
+                                </Button>
+                            </Grid>
+                        </Grid>
                     </CardContent>
                 </Card>
             </div>
